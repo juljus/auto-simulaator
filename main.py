d8980517bf8e387594cd708ef594d148e7e46b91
Haavi <jortin@easyfeedbacktoken.io>
2023-12-09 11:29:26 +0200

    With some tuning

--- main.py
+++ main.py
@@ -2,62 +2,85 @@ import serial
 import pyautogui
 import time
 
-serial_port = 'COM14' # Siia peab kirjutama oma arduino porti. Hiljem võib ka testida, find_arduino.py abil, mis port see on.
+# Siia peab kirjutama oma arduino porti. Hiljem võib ka testida, find_arduino.py abil, mis port see on.
+serial_port = 'COM14'
 baud_rate = 9600
 
 # Tee jada side lahti
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
+
 def process_message(message):
     try:
+        # print(message.decode().strip())
         number, function = message.decode().strip().split(' ')
         try:
-            delay = 1/float(number) 
+            delay = float(number)
         except:
             delay = 0
 
         # TODO! Lisada teised funktsioonid
+        fixing_negative = 1
+        fixing_positive = 1
         key = ''
         if function == 'r':
-            if delay<0:
-                key = 'right'
+            if delay < 0:
+                key = 'a'
+                delay *= fixing_positive
             elif delay > 0:
-                key = 'left'
+                key = 'd'
+                delay *= fixing_negative
         elif function == 'p':
-            key = 'down'
+            key = 's'
+        elif function == 'g':
+            key = 'w'
         else:
             key = None
         return abs(delay), key
-            
+
     except ValueError:
-        print("Invalid message format. Frequency should be a number.")
         return None, None
+
 
 if __name__ == "__main__":
     try:
         multiplier = 1
+        delay_threshold = 30
         last_update = time.time()
-        last_key = None
+        last_key = 'a'
         while True:
             message = ser.readline()
-            
+
             delay, key = process_message(message)
+            if delay == None or key == None:
+                pass
             try:
-                if last_update + delay*multiplier < time.time():
-                    pyautogui.press(key)
-                    # peab veel proovima kuidas seda paremini teha keyup/down kasutades
-                    # if last_key is not None:
-                    #     pyautogui.keyUp(last_key)
-                    # pyautogui.keyDown(key)
+                # if last_update + delay*multiplier < time.time():
+                # pyautogui.press(key)
+                # peab veel proovima kuidas seda paremini teha keyup/down kasutades
+                print(delay, key, last_key)
+                if key == 'w':
+                    pyautogui.keyDown(key)
+                    pyautogui.keyUp('s')
+                elif key == 's':
+                    pyautogui.keyDown(key)
+                    pyautogui.keyUp('w')
+                elif last_key is not None and delay < delay_threshold:
+                    pyautogui.keyUp(last_key)
                     last_key = key
-                    last_update = time.time()
-                    ser.flush() # To avoid buffer overflow
+                if last_key != key:
+                    pyautogui.keyDown(key)
+                # last_update = time.time()
+                ser.flush()  # To avoid buffer overflow
+                ser.reset_input_buffer()  # To avoid buffer overflow
+                ser.reset_output_buffer()  # To avoid buffer overflow
             except KeyboardInterrupt:
                 print("KeyboardInterrupt: Stopping the script.")
                 break
             except:
-                print("No key to release")
-            
+                # print("No key to release")
+                pass
+
     except KeyboardInterrupt:
         print("KeyboardInterrupt: Stopping the script.")
     finally:
