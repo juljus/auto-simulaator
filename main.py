71f42770750a01d6909d9a142cac981fe3d58f16
Juljus <juljus@gmail.com>
2023-12-09 02:36:16 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/juljus/AutoM-ng""
    This reverts commit c9a4224a4b5063564f95624ac29dc4e1c622485e.

--- main.py
+++ main.py
@@ -10,15 +10,18 @@ ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
 def process_message(message):
     try:
-        # print(message)
-        # print(message.decode().strip())
-        delay, function = message.decode().strip().split(' ')
-        delay = float(delay) 
-        
+        number, function = message.decode().strip().split(' ')
+        try:
+            delay = 1/float(number) 
+        except:
+            delay = 0
+
         # TODO! Lisada teised funktsioonid
-        key = 'right'
+        key = ''
         if function == 'r':
             if delay<0:
+                key = 'right'
+            elif delay > 0:
                 key = 'left'
         elif function == 'p':
             key = 'down'
@@ -26,13 +29,15 @@ def process_message(message):
         #     key = 'up'
         else:
             key = None
-        return delay, key
+        return abs(delay), key
             
     except ValueError:
         print("Invalid message format. Frequency should be a number.")
+        return None, None
 
 if __name__ == "__main__":
     try:
+        multiplier = 1
         last_update = time.time()
         last_key = None
         while True:
@@ -40,12 +45,18 @@ if __name__ == "__main__":
             
             delay, key = process_message(message)
             try:
-                if last_update + delay < time.time():
-                    if last_key is not None:
-                        pyautogui.keyUp(last_key)
-                    pyautogui.keyDown(key)
+                if last_update + delay*multiplier < time.time():
+                    pyautogui.press(key)
+                    # peab veel proovima kuidas seda paremini teha keyup/down kasutades
+                    # if last_key is not None:
+                    #     pyautogui.keyUp(last_key)
+                    # pyautogui.keyDown(key)
                     last_key = key
                     last_update = time.time()
+                    ser.flush() # To avoid buffer overflow
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt: Stopping the script.")
+                break
             except:
                 print("No key to release")
             
