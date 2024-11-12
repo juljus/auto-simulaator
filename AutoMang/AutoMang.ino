9f4d4b0c4a015b6202ebf8c54003cf0a00f250a9
Juljus <juljus@gmail.com>
2024-11-12 18:28:56 +0200

    2024

--- AutoMang/AutoMang.ino
+++ AutoMang/AutoMang.ino
@@ -2,6 +2,7 @@
 
 #define MIN_ADDRESS 0
 #define MAX_ADDRESS 100
+#define MID_ADDRESS 200
 int rool = A0;
 int gaas = 7;
 int pidur = 3;
@@ -9,13 +10,17 @@ int nupp = 2;
 int nupp_vajutused = 0;
 int rool_min = 0;
 int rool_max = 1024;
+int cal_min;
+int cal_max;
+int cal_mid;
 
 int disable_nupp = 5;
 int disabled = 0;
 
 void setup() {
-  EEPROM.get(MIN_ADDRESS, rool_min);
-  EEPROM.get(MAX_ADDRESS, rool_max);
+  EEPROM.get(MIN_ADDRESS, cal_min);
+  EEPROM.get(MAX_ADDRESS, cal_max);
+  EEPROM.get(MID_ADDRESS, cal_mid);
 
   Serial.begin(9600);
   Serial.println("Starting program");
@@ -28,15 +33,64 @@ void setup() {
   pinMode(gaas, INPUT_PULLUP);
   pinMode(pidur, INPUT_PULLUP);
   pinMode(nupp, INPUT_PULLUP);
-  delay(5000);
+}
 
+void Calibrate()
+{
+  while (nupp_vajutused <= 2)
+  {
+    int nupp_v = digitalRead(nupp);
+    
+
+    if (nupp_v == 0)
+    {
+      switch (nupp_vajutused)
+      {
+      case 0:
+        cal_min = analogRead(rool);
+
+        EEPROM.put(MIN_ADDRESS, cal_min);
+
+        Serial.println(cal_min);
+        Serial.println("Written to EEPROM min");
+        break;
+
+      case 1:
+        cal_max = analogRead(rool);
+
+        EEPROM.put(MAX_ADDRESS, cal_max);
+
+        Serial.println(cal_max);
+        Serial.println("Written to EEPROM min");
+        break;
+
+      case 2:
+        cal_mid = analogRead(rool);
+
+        EEPROM.put(MID_ADDRESS, cal_mid);
+
+        Serial.println(cal_mid);
+        Serial.println("Written to EEPROM min");
+        break;
+
+      default:
+        break;
+      }
+
+      nupp_vajutused++;
+      delay(1000);
+    }
+  }
 }
 
 void loop() {
+  if (nupp_vajutused == 0) {
+    Calibrate();
+  }
+
   int rool_v = analogRead(rool);
   int gaas_v = digitalRead(gaas);
   int pidur_v = digitalRead(pidur);
-  int nupp_v = digitalRead(nupp);
 
   if (digitalRead(disable_nupp)) {
 
@@ -53,43 +107,32 @@ void loop() {
     }
   }
 
-  if (nupp_v == 0 && nupp_vajutused == 0) {
-    nupp_vajutused += 1;
+  int rool_mapped;
 
-    rool_min = rool_v;
-    EEPROM.put(MIN_ADDRESS, rool_min);
-    Serial.println(rool_min);
-    Serial.println("Written to EEPROM min");
-    delay(10000);
+  if (rool_v < cal_mid)
+  {
+    rool_mapped = map(rool_v, cal_min, cal_mid, -100, 0);
   }
-  else if (nupp_v == 0 && nupp_vajutused == 1) {
-    nupp_vajutused += 1;
-
-    rool_max = rool_v;
-    EEPROM.put(MAX_ADDRESS, rool_max);
-    Serial.println(rool_max);
-    Serial.println("Written to EEPROM max");
-    delay(10000);
+  else
+  {
+    rool_mapped = map(rool_v, cal_mid, cal_max, 0, 100);
   }
-  else if (nupp_v == 0 && nupp_vajutused > 1) {
-    Serial.println("kalibreerimine on juba lõppenud");
-    delay(10000);
-  }
-
-  int rool_mapped = map(rool_v, rool_min, rool_max, -100, 100);
 
   if (disabled == 0) {
     Serial.print(rool_mapped);
     Serial.println(" r");
 
-    if (gaas_v == 0) {
-      Serial.print("1");
-      Serial.println(" g");
-    }
-
     if (pidur_v == 0) {
       Serial.print("1");
       Serial.println(" p");
+    }
+    else if (gaas_v == 0) {
+      Serial.print("1");
+      Serial.println(" g");
+    }
+    else {
+      Serial.print("1");
+      Serial.println(" n");
     }
   }
   delay(10);
--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import serial
 from pynput.keyboard import Controller
 import time
 
+# Constants
+rool_neutral = 40
+
 # Siia peab kirjutama oma arduino porti. Hiljem võib ka testida, find_arduino.py abil, mis port see on.
-serial_port = '/dev/tty.usbmodem1101'
+serial_port = '/dev/tty.usbmodem101'
 baud_rate = 9600
 
 keyboard = Controller()
@@ -13,91 +16,72 @@ ser = serial.Serial(serial_port, baud_rate, timeout=1)
 
 def process_message(message):
     try:
-        # print(message.decode().strip())
+        print("message: " + message.decode())
         number, function = message.decode().strip().split(' ')
-        try:
-            delay = float(number)
-        except:
-            delay = 0
+        print("number: " + number, "; function: " + function)
 
-        # TODO! Lisada teised funktsioonid
-        # fixing_negative = 1 
-        # fixing_positive = 1
         key = ''
-        if function == 'r':
-            if delay < 0:
-                key = 'a'
-                # delay *= fixing_negative
-            elif delay > 0:
-                key = 'd'
-                # delay *= fixing_positive
-            else:
+        number = int(number)
+        match function:
+            case 'r': 
+                if number < -rool_neutral:
+                    key = 'a'
+                elif number > rool_neutral:
+                    key = 'd'
+                else:
+                    key = 'neutral'
+            case 'p':
+                key = 's'
+            case 'g':
+                key = 'w'
+            case 'n':
                 key = 'up'
-                delay = 0
-        elif function == 'p':
-            key = 's'
-            delay = 0
-        elif function == 'g':
-            key = 'w'
-            delay = 0
-        else:
-            key = None
-        return abs(delay), key
+
+        return key, abs(number)
 
     except ValueError:
+        print("ValueError: Could not split the message.")
         return None, None
 
 
 if __name__ == "__main__":
     try:
-        multiplier = 1
-        delay_threshold = 40
-        last_update = time.time()
-        last_key = 'up'
         while True:
-            #message = ser.readline()
             message = ser.readline()
 
-            delay, key = process_message(message)
-            if delay == None or key == None:
+            key, number = process_message(message)
+
+            if number == None or key == None:
                 pass
             else:
                 try:
-                    # if last_update + delay*multiplier < time.time():
-                    # keyboard.press(key)
-                    # peab veel proovima kuidas seda paremini teha release/down kasutades
-                    #print(delay, key, last_key)
-                    if key == 'w':
-                        keyboard.press(key)
-                        keyboard.release('s')
-                    elif key == 's':
-                        keyboard.press(key)
-                        keyboard.release('w')
-                    elif delay > delay_threshold:
-                        if key == 'd':
-                            #keyboard.press(key)
-                            keyboard.press(key)
-                            keyboard.release('a')
-                            last_key = key
-                        elif key == 'a':
-                            #keyboard.press(key)
+                    print("key: " + key, "; number: " + str(number))
+                    match key:
+                        case 'a':
                             keyboard.press(key)
                             keyboard.release('d')
-                            last_key = key
-                    else:
-                        keyboard.release('a')
-                        keyboard.release('d')
-                        #print(key)
+                        case 'd':
+                            keyboard.press(key)
+                            keyboard.release('a')
+                        case 'neutral':
+                            keyboard.release('a')
+                            keyboard.release('d')
+                        case 's':
+                            keyboard.press(key)
+                            keyboard.release('w')
+                        case 'w':
+                            keyboard.press(key)
+                            keyboard.release('s')
+                        case 'up':
+                            keyboard.release('w')
+                            keyboard.release('s')
+                        case _:
+                            pass
 
-                    # last_update = time.time()
-                    #ser.flush()  # To avoid buffer overflow
-                    #ser.reset_input_buffer()  # To avoid buffer overflow
-                    #ser.reset_output_buffer()  # To avoid buffer overflow
                 except KeyboardInterrupt:
                     print("KeyboardInterrupt: Stopping the script.")
                     break
                 except:
-                    # print("No key to release")
                     pass
 
     except KeyboardInterrupt:
